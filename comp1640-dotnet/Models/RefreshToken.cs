0c7d7085d40bf2ac36323f2ad7364053af036f58
long-ngo-hoang <longnh122001@gmail.com>
2023-02-17 05:37:10 +0700

    implement-JWT-authentication-and-refresh-tokens

---
+++ comp1640-dotnet/Models/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace comp1640_dotnet.Models
+{
+	public class RefreshToken
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+
+		//relations
+		[ForeignKey("User")]
+		public string UserId { get; set; }
+		public User? User { get; set; }
+
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+		public string Token { get; set; } = String.Empty;
+		public DateTime TokenCreated { get; set; } = DateTime.Now;
+		public DateTime TokenExpires { get; set; } = DateTime.Now;
+	}
+}
