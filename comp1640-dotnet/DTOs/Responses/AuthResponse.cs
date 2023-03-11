bfaa7e83d164871a115ed93656a715ab91c879a9
Long Ngo <116146731+long-ngo-hoang@users.noreply.github.com>
2023-03-11 18:55:40 +0700

    Update login and refresh token

---
+++ comp1640-dotnet/DTOs/Responses/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿using comp1640_dotnet.Models;
+
+namespace comp1640_dotnet.DTOs.Responses
+{
+	public class AuthResponse
+	{
+		public string Token { get; set; } = string.Empty;
+		public string? RefreshToken { get; set; }
+	}
+}
